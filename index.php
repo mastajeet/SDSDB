7764e71f541d24823f4f65f2340aded2e5b7c153
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2014-07-07 22:59:14 -0400

    Commit d'un portail d'ouverture

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+
+<head>
+<title>Plateforme de test d'objet de SDSDB</title>
+</head>
+
+<body>
+
+<?
+
+
+include_once("/view/pages/template.php");
+
+
+
+?>
+
+
+</body>
+
+
+
+
+</html>
---
+++ view/page/template.php
@@ -0,0 +1,9 @@
+<table width="800">
+    <tr>
+        <td width="200">Menu</td>
+        <td width="600">Content </td>
+    </tr>
+
+
+
+</table>
